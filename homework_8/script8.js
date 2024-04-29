edcfd155adfd76812219612e078ef3888e9665be
Ann <72548732+anni9191@users.noreply.github.com>
2024-04-29 20:34:07 +0200

    Update script8.js due to the task

--- homework_8/script8.js
+++ homework_8/script8.js
@@ -1,9 +1,16 @@
-function myFunctionIsNaN(value) {
-    //Проверяем является ли значение, которое мы передаем не числом, т е нашим NaN 
-    return typeof value === "number" && isNaN(value);
+function myFuncIsNaN(value) {
+
+    // Перевірка, чи аргумент є числом
+    if (typeof value !== 'number') {
+        return false;
+    }
+    
+    // Перевірка, чи є аргумент NaN
+    // NaN не дорівнює нічому, навіть самому собі, тому NaN !== NaN
+    return value !== value;
 }
 
-//Например, как мы можем использовать:
-console.log(myFunctionIsNaN("1988")); // false
-console.log(myFunctionIsNaN(NaN)); //true
-console.log(myFunctionIsNaN("something")); // false
+// Напріклад
+console.log(myFuncIsNaN(NaN)); // true
+console.log(myFuncIsNaN("someinfo")); // false
+console.log(myFuncIsNaN(1988)); // false
